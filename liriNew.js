4b8c996e77b5930a55de099cfda5725a9d112254
JuanD1425 <JuanD1425@me.com>
2019-11-03 13:57:51 -0500

    final

--- liriNew.js
+++ liriNew.js
@@ -1,70 +1,170 @@
-// require("dotenv").config();
-// const moment = require('moment');
-// const keys = require("./keys.js");
-// const Spotify = require("node-spotify-api");
-// const spotify = new Spotify(keys.spotify);
-// const axios = require("axios");
-const prompts =  require('prompts');
+require("dotenv").config();
+const moment = require('moment');
+const keys = require("./keys.js");
+const Spotify = require("node-spotify-api");
+const spotify = new Spotify(keys.spotify);
+const axios = require("axios");
+const ask = require('inquirer');
+// const prompts =  require('prompts');
 const fs = require('fs');
 
-const questions = [{
-  type: 'select',
+function rf() {
+    fs.readFile('./random.txt','utf8',function(err, data){
+        if (err){
+            return console.log(err);
+        }
+        let arrayD = data.split(',');
+            for(let i = 0; i < arrayD.length; i++) {
+                console.log(arrayD[i])
+            }
+            let command = arrayD[0];
+            let query = arrayD[1];
+        if (command[0] === 'concert-this'){
+            concert(query);
+        } else if (command === 'spotify-this-song') {
+            spot(query);
+        }else if(command === 'movie-this'){
+            movie(query);
+        }else{
+            console.log('Sorry something went wrong, restart')
+        }
+    });
+}
+const searchTopic = {
+  type: 'list',
   name: 'command',
-  message: 'Please pick one of the categories to search.',
-  choices: [
-    {title: 'Concerts by Artist', value: 'concert-this'},
-    {title: 'Songs by Track Title', value: 'spotify-this-song'},
-    {title: 'Movie by Title', value: 'movie-this'},
-    {title: 'Do What It Says', value: 'do-what-it-says'}
-],
-  initial: 0
-},
-  {
-    type: prev => prev  === 'concert-this' ? 'text' : null,
-    value: 'query',
-    message: "Enter Artist's Name:"
-  },
-  {
-    type: prev => prev  === 'spotify-this-song' ? 'text' : null,
-    value: 'query',
-    message: 'Enter Song Title:'
-  },
-  {
-    type: prev => prev  === 'movie-this' ? 'text' : null,
-    value: 'query',
-    message: 'Enter Movie Title:'
-  }];
+  message: 'Please select one of these topics to search:',
+  choices: ['concert-this', 'spotify-this-song', 'movie-this', 'do-what-it-wants']
+};
 
-let answers = AsyncFunction();{
-    try {
-    const response = await prompts(questions);
-    let command = response.command;
-    let input = response.query;
-    return { command, input}
-    } catch (e) {
-    throw (e);
-  }
+function concert (response){
+    let query = response.replace(/ /gi, "");
+    let bandsURL = "https://rest.bandsintown.com/artists/" + query + "/events?app_id=codingbootcamp";
+    axios
+        .get(bandsURL)
+        .then(function(response) {
+            let info = response.data;
+            // console.log(info);
+            for (let i = 0; i < info.length; i++) {
+                let eventTime = moment(info[i].datetime).format("MM/DD/YY");
+                let place = info[i].venue;
+                let venue = place.name;
+                let location = "\n\nLocation: " + place.city + ", " + place.region + "\n          " + place.country;
+                console.log("==================================\n\n   Venue: " + venue + location + "\n\n     Date: " + eventTime);
+            }
+        })
+        .catch(function(err) {
+            console.log(err)
+        });
 }
-)
-(async function readFile() {
-  const file = await fs.readFile('./random.txt', 'utf8', function(data, err){
-    let first = data.indexOf('"');
-    let last = data.lastIndexOf('"');
-    let middle = data.indexOf(',');
-    const fileCommand = data.splitText(first + 1, middle - 1), fileQuery = data.splitText(middle + 1, last - 1);
-    let newVar;
-    newVar = {
-      command: fileCommand,
-      query: fileQuery
-    };
-    return newVar;
-  })
-})();
+function spot(ans){
+    spotify.search({
+        type: 'track',
+        query: ans
+    })
+        .then(function (response) {
+            let info = response.tracks.items[0];
+            let artistName = info.album["artists"][0]["name"];
+            let songID = info.album["artists"][0]["id"];
+            let songName = info.name;
+            let albumName = info.album["name"];
+            let previewURL = info.preview;
 
-if (answers.command !== 'do-what-it-says') {
-  let promise = answers(questions)
-      .then().catch();
-} else {
-  let response1 = readFile();
-  console.log(response1);
+            spotify
+                .request('https://api.spotify.com/v1/tracks/' + songID)
+                .then(function(data) {
+                    console.log(data);
+                })
+                .catch(function(err) {
+                    console.error('Error occurred: ' + err);
+                });
+            console.log("\n\nArtist(s): " + artistName + "\n\nSong: " + songName + "\n\nAlbum: " + albumName + "\n\nPreview: " + previewURL + "\n\n")
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
 }
+function movie(query){
+    let userInput = query.replace(/ /gi, "+");
+    const omdbUrl = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&apikey=trilogy";
+    axios.get(omdbUrl)
+        .then(function (response) {
+            // console.log(response);
+            let info = response.data;
+            let movieTitle = info.Title;
+            let movieYear = info.Year;
+            let imdbRating = info.Ratings[0].Value;
+            let rottenRating = info.Ratings[1].Value;
+            let country = info.Country;
+            let lang = info.Language;
+            let actors = info.Actors;
+            let plot = info.Plot;
+
+
+            console.log("\n\n    Movie Title: " + movieTitle + "\n\n   Release Year: " + movieYear + "\n\n    IMDB Rating: " + imdbRating + "\n\nRotten Tomatoes: " + rottenRating + "\n\n        Country: " + country + "\n\n       Language: " + lang + "\n\n         Actors: " + actors + "\n\n           Plot: " + plot + "\n\n");
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+}
+
+function searchQuery() {
+    ask
+        .prompt(searchTopic)
+        .then(function(answer){
+            let ans = answer.command;
+            searches(ans);
+        })
+        .catch(err => console.log(err))
+}
+
+function searches(answer){
+    switch (answer) {
+      case 'concert-this':
+        ask
+            .prompt({
+              type: 'input',
+              name: 'artist',
+              message: 'Please type the name of the artist you would like to see:'
+            }).then(function(response){
+
+                concert(response.artist)})
+
+            .catch(function(err) {
+              console.log(err)
+            });
+        break;
+      case 'spotify-this-song':
+        ask
+            .prompt({
+              type: 'input',
+              name: 'query',
+              message: 'Please type the title of the song:'
+            }).then(function(response){
+         spot(response.query);
+        })
+        .catch(function(err) {
+            console.log(err);
+          });
+
+        break;
+      case 'movie-this':
+          ask
+              .prompt({
+                type: 'input',
+                name: 'query',
+                message: 'Please type the title of the movie:'
+              }).then(function(response) {
+
+                movie(response.query)})
+                .catch(function (err) {
+                  console.log(err)
+                });
+
+          break;
+        case 'do-what-it-wants':
+           rf();
+
+    }
+    }
+searchQuery();
